f815c22d5b003346cafac4b6767bfecbb0a25d01
Nivaspexs <n.vasquez06@ufromail.cl>
2026-07-26 18:42:08 -0400

    Se hacen test para base de datos JSON y DOM a partir del JSON

--- docs/notas.md
+++ docs/notas.md
@@ -26,24 +26,84 @@ Es una arreglo de objetos clave valor. Podemos pasar esto directamente a JSON pl
 const ASIGNATURAS = [{"codigo": "ING050", "nombre": "Ingeniería y Sociedad", "creditos": "4.0", "nivel": 1, "prereqs": []}, ...];
 ```
 
+## Funcionamiento del grid en la malla HTML
+
+El grid está hecho en CSS Grid, desde
+
+
 ## Pasar a JSON
 
 Para pasar a un JSON, quizá la estructura ideal sea:
 
-```
+```json
 {
-  "facultad": "",
-  "carrera: "",
+  "facultad": "Facultad de Ingeniería y Ciencias",
+  "carrera: "Ingeniería Civil Telemática",
   "malla":{
     "nivel1":
     [
       {
-      "ramo": "",
-      "sct":    ,
-      "descripcion": "",
+      "ramo_nombre_html": "Ingeniería<br>y Sociedad",
+      "codigo":"ING050", 
+      "sct": 4   ,
+      "descripcion": "Primer ramo del LIFIC",
       "pre-requisitos": []
       }
     ]
   }
 }
 ```
+
+Ahora voy a intentar cargar desde el JSON este ramo para que se visualice.
+
+```js
+            <div class="course-box" style="grid-column: 1; grid-row: 3;">
+                <div class="course-name">Ingeniería<br>y Sociedad</div>
+                <div class="course-sct">SCT: 4</div>
+            </div>
+```
+
+### Código DOM
+
+Este código permite que al cargar la página cargue o ejecute lo que está dentro
+
+```js
+document.addEventListener('DOMContentLoaded', () => {
+});
+```
+
+Primero entonces hay que cargar el JSON:
+
+```js
+async function cargarJSON() {
+  const respuesta = await fetch('malla_test.json');
+  const malla = await respuesta.json();
+  console.log(malla);
+}
+```
+
+Y se llama en el DOMContentLoaded, pero ahora hay que posicionar los datos de la clase 'course-box', antes lo hacía ingresándolo manual como esto:
+
+```js
+  const nuevoDiv = document.createElement('div');
+  nuevoDiv.classList.add('course-box');
+  nuevoDiv.id = 'ING050';
+  // Inyectamos la estructura interna
+    nuevoDiv.innerHTML = `
+      <div class="course-name">Ingeniería<br>en coito</div>
+      <div class="course-sct">SCT: 69</div>
+    `;
+  nuevoDiv.style.gridColumn = '1';
+  nuevoDiv.style.gridRow = '8';
+```
+
+Y se posiciona dentro del Grid así:
+
+```js
+ const contenedor = document.querySelector('.curriculum-grid');
+  contenedor.appendChild(nuevoDiv);
+```
+
+#### DOM + JSON
+
+El JSON está estructurado que por cada nivel
--- test/script_test.js
+++ test/script_test.js
@@ -1,8 +1,6 @@
 // TEST para animaciones
 
-const tarjetas = document.querySelectorAll('div.course-box');
-
-document.addEventListener('mouseover', (event) => {
+  document.addEventListener('mouseover', (event) => {
   // 1. Buscamos si el clic fue en un .course-box
   const courseBox = event.target.closest('.course-box');
 
@@ -23,3 +21,114 @@ document.addEventListener('mouseover', (event) => {
     }, 200);
   }
 });
+
+// TEST animacion para barra de titulación
+
+
+
+document.addEventListener('mouseover', (event) => {
+  // 1. Buscamos si el clic fue en un .course-box
+  const barraLicenciatura = event.target.closest('.licenciado-bar');
+
+  if (barraLicenciatura) {
+    // Si ya tiene el efecto en curso, no volvemos a activarlo
+    if (barraLicenciatura.classList.contains('click-effect')) return;
+
+    // 2. Agregamos la clase que agranda el div
+    barraLicenciatura.classList.add('click-effect');
+
+    // 3. Quitamos la clase exacto después de 1 segundo (1000 milisegundos)
+    setTimeout(() => {
+      barraLicenciatura.classList.remove('click-effect');
+    }, 200);
+  }
+});
+
+// TEST animacion ramo titulación
+
+
+document.addEventListener('mouseover', (event) => {
+  // 1. Buscamos si el clic fue en un .course-box
+  const ramoTitulacion = event.target.closest('.titulacion-box');
+
+  if (ramoTitulacion) {
+    // Si ya tiene el efecto en curso, no volvemos a activarlo
+    if (ramoTitulacion.classList.contains('click-effect')) return;
+
+    // 2. Agregamos la clase que agranda el div
+    ramoTitulacion.classList.add('click-effect');
+
+    // 3. Quitamos la clase exacto después de 1 segundo (1000 milisegundos)
+    setTimeout(() => {
+      ramoTitulacion.classList.remove('click-effect');
+    }, 200);
+  }
+});
+
+
+// TEST DOM: Creacion de una caja de ramo
+
+document.addEventListener('DOMContentLoaded', () => {
+  const nuevoDiv = document.createElement('div');
+  nuevoDiv.classList.add('course-box');
+  nuevoDiv.id = 'ING050';
+  // Inyectamos la estructura interna
+    nuevoDiv.innerHTML = `
+      <div class="course-name">Ingeniería<br>en coito</div>
+      <div class="course-sct">SCT: 69</div>
+    `;
+  nuevoDiv.style.gridColumn = '1';
+  nuevoDiv.style.gridRow = '8';
+
+  const contenedor = document.querySelector('.curriculum-grid');
+  contenedor.appendChild(nuevoDiv);
+
+  cargarJSON();
+});
+
+// TEST JSON
+
+async function cargarJSON() {
+  //Carga el JSON
+  const respuesta = await fetch('malla_test.json');
+
+  //Aqui convierte el JSON a objeto automáticamente
+  const malla = await respuesta.json();
+  console.log(malla.malla);
+
+  // Carga un array de Strings de las Keys, pero es un objeto así que a los
+  // nombres de los parámetros
+  const clavesMalla = Object.keys(malla.malla);
+  console.log(clavesMalla[0]);
+
+  Object.entries(malla.malla).forEach(([claveNivel, ramos]) => {
+    const numeroColumna = parseInt(claveNivel.match(/\d+/)[0], 10);
+    console.log(numeroColumna);
+
+    const filaInicial = 3;
+    ramos.forEach((ramo, index) => {
+      const numeroFila = index + filaInicial;
+      console.log(ramo.ramo_nombre_html);
+      const nuevoDiv = document.createElement('div');
+      nuevoDiv.classList.add('course-box');
+      nuevoDiv.id = ramo.codigo;
+      // Inyectamos la estructura interna
+      nuevoDiv.innerHTML = `
+        <div class="course-name">`+ ramo.ramo_nombre_html +
+        `</div><div class="course-sct">SCT: ` + ramo.sct +`</div>
+      `;
+      nuevoDiv.style.gridColumn = numeroColumna;
+      nuevoDiv.style.gridRow = numeroFila;
+
+      const contenedor = document.querySelector('.curriculum-grid');
+      contenedor.appendChild(nuevoDiv);
+
+    });
+
+
+  });
+}
+
+function crearRamo(nombre_html, codigo, sct, columna, fila){
+
+}
